55958f7d17af5134757a7b60452f6e29e8498cd9
Abel <abel.tarazona@urp.edu.pe>
2021-05-20 20:32:38 -0500

    ImageView, Edittext y botones

--- app/src/main/java/com/abeltarazona/test1/MainActivity.kt
+++ app/src/main/java/com/abeltarazona/test1/MainActivity.kt
@@ -2,7 +2,9 @@ package com.abeltarazona.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,14 +17,28 @@ class MainActivity : AppCompatActivity() {
         - Int -> 1,2,3...10000
         - Double -> 0.3, 1.5, ...
         - Boolean -> true, false
-        - String -> "Abel", "mesa", "Perú", ....
+        - String -> "Abel", "mesa", "Perú", "12310293" ....
          */
 
         val name : String = "Roberto"
 
-        //val titleTextView : TextView = findViewById(R.id.tvTitleBienvenida)
+        val tvTitleWelcome : TextView = findViewById(R.id.tvTitleWelcome)
+        tvTitleWelcome.text = "Bienvenido, $name"
 
-        //titleTextView.text = "Bienvenido $name a la programación"
+        val btnTrue : Button = findViewById(R.id.btnTrue)
+
+        // CTRL + ESPACIO > Abrir sugerencias
+        // Lambdas - Prog. Funcional
+        // Builder
+        btnTrue.setOnClickListener {
+            Toast.makeText(this, "¡Ganaste!", Toast.LENGTH_LONG).show()
+        }
+
+        val btnFalse: Button = findViewById(R.id.btnFalse)
+
+        btnFalse.setOnClickListener {
+            Toast.makeText(this, "¡Perdiste! \uD83D\uDE1E", Toast.LENGTH_LONG).show()
+        }
 
 
 
