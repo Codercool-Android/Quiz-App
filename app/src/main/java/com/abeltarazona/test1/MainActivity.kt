45712a3816628ae639fa89b84561b24e2cfe0cc2
Abel <abel.tarazona@urp.edu.pe>
2021-06-01 11:46:15 -0500

    Data class creation

--- app/src/main/java/com/abeltarazona/test1/MainActivity.kt
+++ app/src/main/java/com/abeltarazona/test1/MainActivity.kt
@@ -2,6 +2,8 @@ package com.abeltarazona.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -13,8 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicializando mi lista
+        val listQuestions = listOf<Question>(
+            Question("Canberra es la capital de Australia", true),
+            Question("El Canal de Suez conecta el Mar Rojo y el Oceano Indico", true),
+            Question("El inicio del \"Río Nilo\" es en Egipto", false),
+            Question("El Río Amazonas es el más grande de America", true),
+        )
+
         // Inicializando variables locales
         var nameUser : String = ""
+        var currentIndex : Int = 0
 
         // Inicializando componentes
         val tvTitleWelcome : TextView = findViewById(R.id.tvTitleWelcome)
@@ -22,6 +33,11 @@ class MainActivity : AppCompatActivity() {
         val btnFalse: Button = findViewById(R.id.btnFalse)
         val btnIngresar : Button = findViewById(R.id.btnIngresar)
         val etName : EditText = findViewById(R.id.etName)
+        val tvPlayingTitle : TextView = findViewById(R.id.tvPlayingTitle)
+        val tvTitleQuestion : TextView = findViewById(R.id.tvTitleQuestion)
+        val btnNext : Button = findViewById(R.id.btnNext)
+
+
 
         // Inicializando listeners
         btnTrue.setOnClickListener {
@@ -33,9 +49,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnIngresar.setOnClickListener {
+
             nameUser = etName.text.toString()
-            tvTitleWelcome.text = "Usuario jugando: $nameUser"
+
+            if (nameUser.isEmpty()) {
+                Toast.makeText(this, "Debe ingresar un nombre", Toast.LENGTH_SHORT).show()
+            } else {
+                tvPlayingTitle.visibility = View.VISIBLE
+                tvTitleWelcome.text = nameUser
+            }
         }
+
+        btnNext.setOnClickListener {
+            currentIndex = (currentIndex + 1) % listQuestions.size
+            Log.d("Codercool", "Index: $currentIndex")
+            tvTitleQuestion.text = listQuestions[currentIndex].title
+        }
+
+
     }
 
     fun evaluateNameUser(name: String, isCorrect: Boolean) {
---
+++ app/src/main/java/com/abeltarazona/test1/Question.kt
@@ -0,0 +1,9 @@
+package com.abeltarazona.test1
+
+/**
+ * Created by AbelTarazona on 27/05/2021
+ */
+data class Question(
+    val title : String,
+    val isCorrect : Boolean
+)
