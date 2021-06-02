532d88e8860197537cfbc1f730bef4ed3e9838c0
Abel <abel.tarazona@urp.edu.pe>
2021-06-01 20:14:35 -0500

    Logica de preguntas

--- app/src/main/java/com/abeltarazona/test1/MainActivity.kt
+++ app/src/main/java/com/abeltarazona/test1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.abeltarazona.test1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,8 +7,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+enum class TypeSelector {
+    TRUE,
+    FALSE
+}
 
 class MainActivity : AppCompatActivity() {
+
+    var questionCorrect: Int = 0
+    var questionIncorrect: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,35 +25,45 @@ class MainActivity : AppCompatActivity() {
 
         // Inicializando mi lista
         val listQuestions = listOf<Question>(
-            Question("Canberra es la capital de Australia", true),
+            Question("Canberra es la capital de Australia", false),
             Question("El Canal de Suez conecta el Mar Rojo y el Oceano Indico", true),
             Question("El inicio del \"Río Nilo\" es en Egipto", false),
             Question("El Río Amazonas es el más grande de America", true),
         )
 
         // Inicializando variables locales
-        var nameUser : String = ""
-        var currentIndex : Int = 0
+        var nameUser: String = ""
+        var currentIndex: Int = 0
 
         // Inicializando componentes
-        val tvTitleWelcome : TextView = findViewById(R.id.tvTitleWelcome)
-        val btnTrue : Button = findViewById(R.id.btnTrue)
+        val tvTitleWelcome: TextView = findViewById(R.id.tvTitleWelcome)
+        val btnTrue: Button = findViewById(R.id.btnTrue)
         val btnFalse: Button = findViewById(R.id.btnFalse)
-        val btnIngresar : Button = findViewById(R.id.btnIngresar)
-        val etName : EditText = findViewById(R.id.etName)
-        val tvPlayingTitle : TextView = findViewById(R.id.tvPlayingTitle)
-        val tvTitleQuestion : TextView = findViewById(R.id.tvTitleQuestion)
-        val btnNext : Button = findViewById(R.id.btnNext)
-
+        val btnIngresar: Button = findViewById(R.id.btnIngresar)
+        val etName: EditText = findViewById(R.id.etName)
+        val tvPlayingTitle: TextView = findViewById(R.id.tvPlayingTitle)
+        val tvTitleQuestion: TextView = findViewById(R.id.tvTitleQuestion)
+        val btnNext: Button = findViewById(R.id.btnNext)
 
 
         // Inicializando listeners
         btnTrue.setOnClickListener {
-            evaluateNameUser(nameUser, true)
+            if (isUserLogged(nameUser)) {
+                isQuestionCorrect(
+                    listQuestions[currentIndex].isCorrect,
+                    TypeSelector.TRUE
+                ) // FALSE, TRUE
+            } else {
+                Toast.makeText(this, "NO EVALUAR PREGUNTA", Toast.LENGTH_SHORT).show()
+            }
         }
 
         btnFalse.setOnClickListener {
-            evaluateNameUser(nameUser, false)
+            if (isUserLogged(nameUser)) {
+                isQuestionCorrect(listQuestions[currentIndex].isCorrect, TypeSelector.FALSE)
+            } else {
+                Toast.makeText(this, "NO EVALUAR PREGUNTA", Toast.LENGTH_SHORT).show()
+            }
         }
 
         btnIngresar.setOnClickListener {
@@ -61,6 +79,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnNext.setOnClickListener {
+
+            if (currentIndex == (listQuestions.size - 1)) {
+                Toast.makeText(
+                    this,
+                    "Resultados finales: Correctas -> $questionCorrect // Incorrectas -> $questionIncorrect",
+                    Toast.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
+            }
+
             currentIndex = (currentIndex + 1) % listQuestions.size
             Log.d("Codercool", "Index: $currentIndex")
             tvTitleQuestion.text = listQuestions[currentIndex].title
@@ -69,18 +97,34 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun evaluateNameUser(name: String, isCorrect: Boolean) {
-        if (name.isEmpty()) {
-            Toast.makeText(this, "Por favor agrega un nombre de usuario", Toast.LENGTH_SHORT).show()
-        } else {
-            if (isCorrect) {
-                Toast.makeText(this, "¡Ganaste!", Toast.LENGTH_SHORT).show()
+    private fun isQuestionCorrect(correct: Boolean, type: TypeSelector) {
+        if (type == TypeSelector.TRUE) {
+
+            if (correct) {
+                questionCorrect++
+                Toast.makeText(this, "Respuesta correcta", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "¡Perdiste! \uD83D\uDE1E", Toast.LENGTH_SHORT).show()
+                questionIncorrect++
+                Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT).show()
+            }
+
+        } else if (type == TypeSelector.FALSE) {
+            if (!correct) {
+                questionCorrect++
+                Toast.makeText(this, "Respuesta correcta", Toast.LENGTH_SHORT).show()
+            } else {
+                questionIncorrect++
+                Toast.makeText(this, "Respuesta incorrecta", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-
+    private fun isUserLogged(name: String): Boolean {
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Por favor agrega un nombre de usuario", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
 
 }
