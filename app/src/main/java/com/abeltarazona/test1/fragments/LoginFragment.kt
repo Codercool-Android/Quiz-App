ae8c406cc26a103f2b7e1a5111ddb7b6a4f92181
Abel <abel.tarazona@urp.edu.pe>
2021-06-08 21:03:59 -0500

    Register Fragment

--- app/src/main/java/com/abeltarazona/test1/fragments/LoginFragment.kt
+++ app/src/main/java/com/abeltarazona/test1/fragments/LoginFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.abeltarazona.test1.R
 
@@ -25,8 +27,32 @@ class LoginFragment : Fragment() {
 
         val btnRegister : TextView = view.findViewById(R.id.tvRegister)
 
+        val btnLogin : Button = view.findViewById(R.id.btnLogin)
+
+        val etUser : EditText = view.findViewById(R.id.editTextTextPersonName)
+
+        val etPassword : EditText = view.findViewById(R.id.editTextTextPassword)
+
         btnRegister.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_loginFragment_to_registerFragment2)
+        }
+
+        btnLogin.setOnClickListener {
+
+            // User -> Redox . Password -> 3214
+            val user = etUser.text.toString()
+            val password = etPassword.text.toString()
+
+            if (user.isEmpty() || password.isEmpty()) {
+                Toast.makeText(context, "Campos vacios", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (user == "Redox" && password == "3214") {
+                Toast.makeText(context, "Inicio de sesión correcto", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, "Revise las credenciales", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
--- app/src/main/java/com/abeltarazona/test1/fragments/RegisterFragment.kt
+++ app/src/main/java/com/abeltarazona/test1/fragments/RegisterFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.abeltarazona.test1.R
 
 
@@ -16,6 +19,23 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val toolbar : Toolbar = view.findViewById(R.id.toolbar)
+
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        toolbar.title = "Registro"
+
+        toolbar.setNavigationOnClickListener {
+            Navigation.findNavController(it).popBackStack()
+        }
+
     }
 
 }
